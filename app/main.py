0ec0769ca3eec0d458f2c7d362d47b1c30296889
hevanli <evanlicubs@gmail.com>
2023-07-13 14:43:34 -0700

    deleted unnecessary comments and import

--- app/main.py
+++ app/main.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 import uvicorn
 
 from api.router import cs_router
 
 app = FastAPI()
-
-# Second endpoint (Does the search thing, used google as example. Not finished yet simplified version anyone can add to this please)
-# To get this to work, run the server and input http://localhost:8000/search?q=searchterm into your explorer. Run "uvicorn main:app --reload" in the terminal first.
-
-from fastapi import Query
-
-# @app.get("/search")
-# async def search(q: str = Query(..., description="")):
-    
-#     results = [
-#         {"title": "Result", "description": "This is the result"},
-#     ]
-
-#     return {"results": results}
 
 app.include_router(cs_router)
 
