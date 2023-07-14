cad93dba97dc633ec8d047c5ff37a77b57fdd95c
hevanli <evanlicubs@gmail.com>
2023-07-14 12:14:23 -0700

    home page

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from api.router import cs_router
@@ -6,6 +7,7 @@ from api.router import cs_router
 app = FastAPI()
 
 app.include_router(cs_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
