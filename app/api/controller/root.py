22cff33289a1addf5d2a27eb2806dd56f066bbb2
Evan Li <67269156+hevanli@users.noreply.github.com>
2023-07-20 12:40:50 -0700

    Update root.py

--- app/api/controller/root.py
+++ app/api/controller/root.py
@@ -2,7 +2,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi import Request
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 
 async def root(req: Request):
     return templates.TemplateResponse("index.html", {"request": req})
