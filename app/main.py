946b5e0c55a88f28c2cdf5ab956ef6097bdaca89
Evan Li <67269156+hevanli@users.noreply.github.com>
2023-07-20 12:39:54 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from api.router import cs_router
+from app.api.router import cs_router
 
 app = FastAPI()
 
 app.include_router(cs_router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
 
 # to run server either type "uvicorn main:app --reload" or "python app/main.py" (assuming you're in the root directory) 
 
