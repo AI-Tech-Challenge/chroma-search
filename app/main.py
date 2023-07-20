2b5e029c97a4ffd90093a4b3a7e6ad904f57dde3
hevanli <evanlicubs@gmail.com>
2023-07-20 15:16:26 -0700

    deployment config + file structure change

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 import uvicorn
 
-from api.router import cs_router
+from app.api.router import cs_router
 
 app = FastAPI()
 
 app.include_router(cs_router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount(
+        "/static", 
+        StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"), 
+        name="static",
+)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
 
 # to run server either type "uvicorn main:app --reload" or "python app/main.py" (assuming you're in the root directory) 
 
