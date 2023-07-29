4e0a0ba8bb46b715fb7ea31aa3bb3ed3df0685de
unknown <sunverse2007@gmail.com>
2023-07-29 14:13:04 -0700

    Modified the Validation process

--- app/validation/Code.py
+++ app/validation/Code.py
@@ -1,13 +1,34 @@
 import nltk
-from nltk import pos_tag
+from nltk import pos_tag, word_tokenize
+from nltk.corpus import wordnet as wn
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('punkt')
 
-def validate_words(input_text):
-    tagged_text = pos_tag(nltk.word_tokenize(input_text))
-    return any(pos.startswith("NN") for word, pos in tagged_text)
+def validate_noun(word):
+ 
+    synsets = wn.synsets(word, pos=wn.NOUN)
+    if synsets:
+      
+        for synset in synsets:
+            if word.lower() in synset.lemma_names():
+                return True
+    return False
 
 def validate_user_input(input_text):
 
-    return validate_words(input_text)
+    inappropriate_words = ["explicit_word", "other_inappropriate_word"]
+
+    nouns = [word for word, pos in pos_tag(word_tokenize(input_text)) if pos.startswith('NN')]
+    if not nouns:
+        return False
+
+    specific_nouns = [noun for noun in nouns if validate_noun(noun.lower()) and noun.lower() not in inappropriate_words]
+
+    if specific_nouns:
+        return True
+    return False
+
 
 user_input = input("Enter your input: ")
 is_valid = validate_user_input(user_input)
