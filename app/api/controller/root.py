1cfad9df85302f7e7d2526ce5c3a1ab11be1a22a
hevanli <evanlicubs@gmail.com>
2023-07-31 12:12:31 -0700

    core logic implemented

--- app/api/controller/root.py
+++ app/api/controller/root.py
@@ -1,11 +1,13 @@
 from fastapi.templating import Jinja2Templates
 from fastapi import Request, Form
 
+from app.core.generate_hexcodes import generate_hexcodes
+
 templates = Jinja2Templates(directory="templates")
 
 async def root(req: Request):
     return templates.TemplateResponse("index.html", {"request": req})
 
 async def submit(req: Request, user_input: str=Form(...)):
-    print(user_input)
-    return templates.TemplateResponse("index.html", {"request": req, "user_input": user_input})
+    hexcodes = generate_hexcodes(user_input)
+    return templates.TemplateResponse("index.html", {"request": req, "user_input": hexcodes})
